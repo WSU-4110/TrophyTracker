3bf3ef670bee0fe9d75fd79f221c2ee09956f0b3
Zavaar Shah <thatziv@gmail.com>
2024-02-01 12:15:42 -0500

    added flowbite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: ["./src/**/*.tsx", "./node_modules/flowbite-react/lib/**/*.js",],
   theme: {
     extend: {
       fontFamily: {
@@ -10,5 +10,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 } satisfies Config;
