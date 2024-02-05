e1677e5faf30b4139426939005a48afd282cdf14
Zavaar Shah <thatziv@gmail.com>
2024-02-05 18:19:33 -0500

    db connection issues

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,15 +1,19 @@
-import mongoose from "mongoose";
+import mongoose, { type ConnectOptions } from "mongoose";
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: false,
+  maxPoolSize: 1,
+  minPoolSize: 1,
+  socketTimeoutMS: 17000,
+  serverSelectionTimeoutMS: 12000,
+  maxIdleTimeMS: 20000,
+};
 
 const connect = async () => {
   try {
     console.log("Connecting to database...");
-    await mongoose.connect(process.env.MONGODB_URI!, {
-      maxPoolSize: 1,
-      minPoolSize: 1,
-      socketTimeoutMS: 17000,
-      serverSelectionTimeoutMS: 12000,
-      maxIdleTimeMS: 20000,
-    });
+    await mongoose.connect(process.env.MONGODB_URI!, options as ConnectOptions);
   } catch (err) {
     console.error(err);
     throw new Error("Cannot connect to database...");
