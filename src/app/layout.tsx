d7e0157085da4d07c75277624e06c25eec06d1b7
SukrutN <sukrutnadigotti@gmail.com>
2024-02-07 22:53:17 -0500

    [refactor]: Update component imports and layout setup in RootLayout
    This commit refactors the RootLayout component by updating component imports and optimizing layout setup for improved performance and maintainability.
    
    Changes included in this commit:
    - Added import for Footer, Header, and ScrollToTop components
    - Imported necessary providers for session management and theme configuration
    - Defined RootLayout component structure with appropriate providers and components
    
    These modifications enhance the structure and organization of the RootLayout, ensuring better scalability and readability of the frontend codebase.

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ScrollToTop from "@/components/ScrollToTop";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+import "../styles/index.css";
+import "../styles/prism-vsc-dark-plus.css";
+import ToasterContext from "./api/contex/ToasetContex";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning={true} className="!scroll-smooth" lang="en">
+      {}
+      <head />
+
+      <body>
+        <SessionProvider>
+          <ThemeProvider
+            attribute="class"
+            enableSystem={false}
+            defaultTheme="light"
+          >
+            <ToasterContext />
+            <Header />
+            {children}
+            <Footer />
+            <ScrollToTop />
+          </ThemeProvider>
+        </SessionProvider>
+      </body>
+    </html>
+  );
+}
