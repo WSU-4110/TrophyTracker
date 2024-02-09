626676d82e9f6874c28435b63d4be538e4a8a981
SukrutN <sukrutnadigotti@gmail.com>
2024-02-09 01:15:01 -0500

    Modify hero to be present on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,16 @@
-import { Hero } from "@/comps/Hero";
+import Hero from "@/components/Hero";
 import { getServerSession } from "next-auth/next";
 import Link from "next/link";
 
-async function Welcome() {
-  const session = await getServerSession();
-  return (
-    <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-      Welcome
-      <span className="text-[hsl(280,100%,70%)]">
-        {session ? ` ${session.user.name}.` : "."}
-      </span>
-    </h1>
-  );
-}
+export const metadata: Metadata = {
+  title: "TrophyTracker",
+  description: "TrophyTracker.",
+};
 
 export default function HomePage() {
   return (
-    <div className="container flex flex-col items-center justify-center gap-12 px-4 md:items-start">
+    <main>
       <Hero />
-    </div>
+    </main>
   );
 }
