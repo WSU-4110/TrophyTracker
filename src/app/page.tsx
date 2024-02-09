354a84712e496009259f3b57244d11757088c964
SukrutN <sukrutnadigotti@gmail.com>
2024-02-09 01:21:08 -0500

    Fix deployment error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,6 @@ import Hero from "@/components/Hero";
 import { getServerSession } from "next-auth/next";
 import Link from "next/link";
 
-export const metadata: Metadata = {
-  title: "TrophyTracker",
-  description: "TrophyTracker.",
-};
-
 export default function HomePage() {
   return (
     <main>
