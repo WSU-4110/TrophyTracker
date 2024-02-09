905774df09abfe035e758b79eeb9da8184c309eb
SukrutN <sukrutnadigotti@gmail.com>
2024-02-09 01:51:56 -0500

    Testing eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,4 @@
-/** @type {import("eslint").Linter.Config} 
+/** @type {import("eslint").Linter.Config} */
 const config = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -35,4 +35,3 @@ const config = {
 };
 
 module.exports = config;
-*/
