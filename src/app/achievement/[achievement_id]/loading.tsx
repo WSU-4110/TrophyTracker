b155ee730c48d06a022e4b90ad89fffcadc54a05
Zavaar Shah <thatziv@gmail.com>
2024-03-06 16:52:51 -0500

    Created specific achievement page
    Co-authored-by: Sukrut Nadigotti <sukrutnadigotti@gmail.com>

---
+++ src/app/achievement/[achievement_id]/loading.tsx
@@ -0,0 +1,9 @@
+import Skeleton from "@/comps/Skeleton";
+
+export default function AchievementLoader() {
+  return (
+    <div className="tt-main-layout p-8">
+      <Skeleton />
+    </div>
+  );
+}
