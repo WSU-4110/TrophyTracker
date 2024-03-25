8b9d57f326b4fb39b013d8b29f504a5cc63308b0
Venkat Yenduri <milkyscripts@gmail.com>
2024-03-25 15:04:16 -0400

    Added new logo, updated several components with enhancements
    Co-authored-by: Sukrut Nadigotti <sukrutnadigotti@gmail.com>
    Co-authored-by: Pierre Tawfik <ptawfik6@gmail.com>

--- src/components/Pricing/index.tsx
+++ src/components/Pricing/index.tsx
@@ -29,8 +29,8 @@ const Pricing = () => {
         <div className="mb-[60px]">
           <SectionTitle
             subtitle="Pricing Table"
-            title="Our Pricing Plan"
-            paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+            title="Completely Free to Use!"
+            paragraph="Trophytracker will be free for everyone to use permanently. We will never charge you for using our platform!"
             center
           />
         </div>
