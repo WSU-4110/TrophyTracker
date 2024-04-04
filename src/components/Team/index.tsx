d91362b6cb9147794aa7d88f5e1e0636930e29a3
Yusef Turfe <hg6453@wayne.edu>
2024-04-04 18:04:57 +0000

    fix sytnax error with team

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -1,69 +1,69 @@
-import { type TeamType } from '@/types/team'
-import SectionTitle from '../Common/SectionTitle'
-import SingleTeam from './SingleTeam'
+import { type TeamType } from "@/types/team";
+import SectionTitle from "../Common/SectionTitle";
+import SingleTeam from "./SingleTeam";
 
 const teamData: TeamType[] = [
   {
     id: 1,
-    name: 'Sukrut Nadigotti',
-    designation: 'Software Developer',
-    image: '/images/team/team-01.png',
-    facebookLink: '/#',
-    twitterLink: '/#',
-    instagramLink: '/#'
+    name: "Sukrut Nadigotti",
+    designation: "Software Developer",
+    image: "/images/team/team-01.png",
+    facebookLink: "/#",
+    twitterLink: "/#",
+    instagramLink: "/#",
   },
   {
     id: 2,
-    name: 'Venkat Yenduri',
-    designation: 'Software Developer',
-    image: '/images/team/team-02.png',
-    facebookLink: '/#',
-    twitterLink: '/#',
-    instagramLink: '/#'
+    name: "Venkat Yenduri",
+    designation: "Software Developer",
+    image: "/images/team/team-02.png",
+    facebookLink: "/#",
+    twitterLink: "/#",
+    instagramLink: "/#",
   },
   {
     id: 3,
-    name: 'Zavaar Shah',
-    designation: 'Software Developer',
-    image: '/images/team/team-03.png',
-    facebookLink: '/#',
-    twitterLink: '/#',
-    instagramLink: '/#'
+    name: "Zavaar Shah",
+    designation: "Software Developer",
+    image: "/images/team/team-03.png",
+    facebookLink: "/#",
+    twitterLink: "/#",
+    instagramLink: "/#",
   },
   {
     id: 4,
-    name: 'Pierre Tawfik',
-    designation: 'Software Developer',
-    image: '/images/team/team-04.png',
-    facebookLink: '/#',
-    twitterLink: '/#',
-    instagramLink: '/#'
-  }
+    name: "Pierre Tawfik",
+    designation: "Software Developer",
+    image: "/images/team/team-04.png",
+    facebookLink: "/#",
+    twitterLink: "/#",
+    instagramLink: "/#",
+  },
   {
     id: 5,
-    name: 'Yusef Turfe',
-    designation: 'Software Developer',
-    image: '/images/team/team-05.png',
-    facebookLink: '/#',
-    twitterLink: '/#',
-    instagramLink: '/#'
-  }
-{
+    name: "Yusef Turfe",
+    designation: "Software Developer",
+    image: "/images/team/team-05.png",
+    facebookLink: "/#",
+    twitterLink: "/#",
+    instagramLink: "/#",
+  },
+  {
     id: 6,
-    name: 'Alberto Cervantes',
-    designation: 'Software Developer',
-    image: '/images/team/team-06.png',
-    facebookLink: '/#',
-    twitterLink: '/#',
-    instagramLink: '/#'
-  }
-]
+    name: "Alberto Cervantes",
+    designation: "Software Developer",
+    image: "/images/team/team-06.png",
+    facebookLink: "/#",
+    twitterLink: "/#",
+    instagramLink: "/#",
+  },
+];
 
 const Team = () => {
   return (
     <section
       id="team"
-      className="bg-gray-1 dark:bg-dark-2 overflow-hidden pb-12 pt-20 lg:pb-[90px] lg:pt-[120px]"
+      className="overflow-hidden bg-gray-1 pb-12 pt-20 dark:bg-dark-2 lg:pb-[90px] lg:pt-[120px]"
     >
       <div className="container">
         <div className="mb-[60px]">
@@ -83,7 +83,7 @@ const Team = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
